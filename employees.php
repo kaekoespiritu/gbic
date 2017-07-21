2e956162649c5dd85d80ddf6a17a8c2c43f13acc
JustineDiza <justinediza@gmail.com>
2017-07-21 20:20:28 +0800

    Removing <<< HEAD text from employees

--- employees.php
+++ employees.php
@@ -38,7 +38,6 @@ include('directives/session.php');
 				</div>
 				<!-- FILTER EMPLOYEE BY POSITION -->
 				<div class="col-md-4 col-md-pull-1 pull-down text-right">
-<<<<<<< HEAD
 					Filter employees by:
 					<div class="btn-group">
 						<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
