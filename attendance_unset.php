b1db5be4d3c6302694e9aa4e465d7cef3c64bebe
Karlo Espiritu <karlo.espiritu@yahoo.com>
2017-09-12 01:28:27 +0800

    holiday can properly cancel

--- attendance_unset.php
+++ attendance_unset.php
@@ -3,9 +3,24 @@ include('directives/session.php');
 
 if(isset($_SESSION['holidayName']) && isset($_SESSION['holidayType']))
 {
+	$holidayDate = $_SESSION['holidayDate'];
+	$holidayChecker = "SELECT * FROM holiday WHERE date = '$holidayDate'";
+	$holidayQuery = mysql_query($holidayChecker);
+	if($holidayQuery)
+	{
+		$holidayRow = mysql_num_rows($holidayQuery);
+		if($holidayRow != 0)
+		{
+			$delHoliday = "DELETE FROM holiday WHERE date = '$holidayDate'";
+			$delHolidayQuery = mysql_query($delHoliday);
+		}
+	}
+	
+
 	unset($_SESSION['holidayName']);
 	unset($_SESSION['holidayType']);
 	unset($_SESSION['holidayDate']);
+
 	Print "<script>window.location.assign('attendance.php')</script>";
 }
 ?>
