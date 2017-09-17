9da818889fff22ff7fbdb8d94bec65a30e5946de
JustineDiza <justinediza@gmail.com>
2017-09-18 01:26:22 +0800

    Added modals for vale

--- payroll.php
+++ payroll.php
@@ -9,6 +9,12 @@ include('directives/session.php');
 
 	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
 	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<style>
+		.well
+		{
+			margin-bottom: 0px !important;
+		}
+	</style>
 </head>
 <body style="font-family: Quicksand;">
 	<!-- 
@@ -24,15 +30,15 @@ include('directives/session.php');
 
 	<div class="row pull-down">
 	<div class="col-md-10 col-md-offset-1">
-			<ol class="breadcrumb text-left">
-				<li><a href="payroll_position.php" class="btn btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Position</a></li>
-				<li class="active">Employees at SITENAME</li>
-				<button class="btn btn-success pull-right" onclick="saveChanges()">Save changes</button>
-			</ol>
-			</div>
+		<ol class="breadcrumb text-left">
+			<li><a href="payroll_position.php" class="btn btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Position</a></li>
+			<li class="active">Employees at SITENAME</li>
+			<button class="btn btn-primary pull-right">Next employee <spen class="glyphicon glyphicon-arrow-right"></spen></button>
+			<button class="btn btn-success pull-right" style="margin-right:5px" onclick="saveChanges()">Save and compute <span class="glyphicon glyphicon-floppy-saved"></span></button>
+		</ol>
+	</div>
 		<div class="col-md-10 col-md-offset-1">
 			<h2 class="text-left">Miguelito Joselito Dela Cruz</h2>
-			<hr>
 			<div class="row">
 				<div class="col-md-8 text-left" style="word-break: keep-all">
 					<h4><b style="font-family: QuickSandMed">Employee ID:</b> 2014-1352845</h4>
@@ -46,7 +52,8 @@ include('directives/session.php');
 					<h4>Has SSS documents</h4>
 				</div>
 			</div>
-			<br>
+		</div>
+		<div class="col-md-10 col-md-offset-1">
 			<table class="table table-bordered table-condensed" style="background-color:white;">
 				<tr>
 					<td colspan="2">Wednesday</td>
@@ -58,79 +65,117 @@ include('directives/session.php');
 					<td colspan="2">Tuesday</td>
 				</tr>
 				<tr>
-					<td>Time In: <div class="well well-sm">8:00AM</div></td>
-					<td>Time Out: <div class="well well-sm">5:00PM</div></td>
-					<td>Time In: <div class="well well-sm">8:00AM</div></td>
-					<td>Time Out: <div class="well well-sm">5:00PM</div></td>
-					<td>Time In: <div class="well well-sm">8:00AM</div></td>
-					<td>Time Out: <div class="well well-sm">5:00PM</div></td>
-					<td>Time In: <div class="well well-sm">8:00AM</div></td>
-					<td>Time Out: <div class="well well-sm">5:00PM</div></td>
-					<td>Time In: <div class="well well-sm">8:00AM</div></td>
-					<td>Time Out: <div class="well well-sm">5:00PM</div></td>
-					<td>Time In: <div class="well well-sm">8:00AM</div></td>
-					<td>Time Out: <div class="well well-sm">5:00PM</div></td>
-					<td>Time In: <div class="well well-sm">8:00AM</div></td>
-					<td>Time Out: <div class="well well-sm">5:00PM</div></td>
+					<td>Time In:  8:00AM </td>
+					<td>Time Out:  5:00PM </td>
+					<td>Time In:  8:00AM </td>
+					<td>Time Out:  5:00PM </td>
+					<td>Time In:  8:00AM </td>
+					<td>Time Out:  5:00PM </td>
+					<td>Time In:  8:00AM </td>
+					<td>Time Out:  5:00PM </td>
+					<td>Time In:  8:00AM </td>
+					<td>Time Out:  5:00PM </td>
+					<td>Time In:  8:00AM </td>
+					<td>Time Out:  5:00PM </td>
+					<td>Time In:  8:00AM </td>
+					<td>Time Out:  5:00PM </td>
 				</tr>
 			</table>
+		</div>
+		<div class="col-md-10 col-md-offset-1">
 			<div class="panel">
 				<table class="table table-bordered table-responsive">
 					<tr>
-						<td>
+						<td style="background-color: peachpuff">
 							<h4>Total hours rendered: 54</h4>
 						</td>
-						<td>
+						<td style="background-color: lemonchiffon">
 							<h4>Total overtime: 0</h4>
 						</td>
-						<td>
+						<td style="background-color: powderblue">
 							<h4>Total night differential: 0</h4>
 						</td>
 					</tr>
 				</table>
 				<div class="row">
-					<form>
-						<div class="col-md-3 col-md-offset-2">
-							<h3 class="text-left">Loans</h3>
-							<table>
-								<tr>
-									<td>SSS</td>
-									<td><input type="text" for="sss" class="form-control"></td>
-								</tr>
-								<tr>
-									<td>Pag-IBIG</td>
-									<td><input type="text" for="pagibig" class="form-control"></td>
-								</tr>
-							</table>
+					<form class="horizontal">
+						<div class="col-md-2 col-md-offset-3">
+							<h4 class="text-left">Loans</h4>
+							<div class="form-group">
+								<label class="control-label col-md-3" for="sss">SSS</label>
+								<div class="col-md-9">
+									<input type="text" id="sss" class="form-control">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="control-label col-md-3" for="pagibig">Pag-IBIG</label>
+								<div class="col-md-9">
+									<input type="text" id="pagibig" class="form-control">
+								</div>
+							</div>
+						</div>
+						<div class="col-md-2">
+							<h4 class="text-left">Deductions</h4>
+							<div class="form-group">
+								<label class="control-label col-md-3" for="tax">Tax</label>
+								<div class="col-md-9">
+									<input type="text" id="tax" class="form-control">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="control-label col-md-3" for="tools">Tools</label>
+								<div class="col-md-9">
+									<input type="text" id="tools" class="form-control">
+								</div>
+							</div>
 						</div>
 						<div class="col-md-3">
-							<h3 class="text-left">Deductions</h3>
-							<table>
-								<tr>
-									<td>Tax</td>
-									<td><input type="text" for="tax" class="form-control"></td>
-								</tr>
-								<tr>
-									<td>Tools</td>
-									<td><input type="text" for="tools" class="form-control"></td>
-								</tr>
-							</table>
-						</div>
-						<div class="col-md-3">
-							<h3 class="text-left">Vale</h3>
-							<table>
-								<tr>
-									<td colspan="2">SAMPLE TEXT HERE...</td>
-								</tr>
-								<tr>
-									<td><button class="btn btn-success">Add new vale</button></td>
-									<td><button class="btn btn-danger">Deduct old vale</button></td>
-								</tr>
-							</table>
+							<h4 class="text-left">Vale</h4>
+								<h4 class="text-left">1,000 PHP</h4>
+								<button type="button" class="btn btn-success col-md-6" data-toggle="modal" data-target="#addVale">Add new vale</button>
+								<button type="button" class="btn btn-danger col-md-6" data-toggle="modal" data-target="#deductVale">Deduct old vale</button>
 						</div>
 					</form>
 				</div>
-				<button class="btn btn-primary pull-down">Next</button>
+				<br>
+				<!-- MODALS -->
+				<div class="modal fade" id="addVale">
+				  <div class="modal-dialog modal-sm" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				        <h4 class="modal-title" id="myModalLabel">Add new vale</h4>
+				      </div>
+				      <div class="modal-body">
+				      	Previous amount: 1,000 PHP
+				        <input type="text" placeholder="Add as new vale">
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				        <button type="button" class="btn btn-primary">Add</button>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+
+				<div class="modal fade" id="deductVale">
+				  <div class="modal-dialog modal-sm" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				        <h4 class="modal-title" id="myModalLabel">Deduct old vale</h4>
+				      </div>
+				      <div class="modal-body">
+				      	Previous amount: 1,000 PHP
+				        <input type="text" placeholder="Deduct from old vale">
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				        <button type="button" class="btn btn-primary">Deduct</button>
+				      </div>
+				    </div>
+				  </div>
+				</div>
 			</div>
 		</div>	
 	</div>
