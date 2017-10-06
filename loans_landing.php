fca053d792487704bedfbb534b0aa5808ddb5e39
JustineDiza <justinediza@gmail.com>
2017-10-06 20:48:16 +0800

    Created a landing page for loans

---
+++ loans_landing.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Payroll</title>
+	<!-- Company Name: Green Built Industrial Corporation -->
+
+	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+</head>
+<body style="font-family: QuicksandMed;">
+	<div class="container-fluid">
+
+	</div>
+</body>
+</html>
