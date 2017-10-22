fe123f971bb6df1df6811fc52661c0524db6571f
JustineDiza <justinediza@gmail.com>
2017-10-22 23:00:02 +0800

    Minor cosmetic tweak for loans history modal

--- directives/modals/loans_history.php
+++ directives/modals/loans_history.php
@@ -1,20 +1,45 @@
 <div class="modal fade" id="viewLoanHistory" role="dialog">
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
-			<div class="modal-header">
-				<div class="col-md-7">
-					<h4 class="modal-title text-right">View employee's loan history</h4>
+				<div class="col-md-12">
+					<h4 class="modal-title pull-down">NAME OF EMPLOYEE's loan history</h4>
 				</div>
-				<div class="col-md-5">
-					<button type="button" class="close" data-dismiss="modal">&times;</button>
-				</div>
-			</div>
-			<div class="modal-body">
+			<div class="modal-body pull-down-more">
 				<table class="table table-bordered">
 					<tr>
 						<td>Date</td>
 						<td>Balance</td>
 						<td>Amount Paid</td>
+					</tr>
+					<tr>
+						<td>DATE DATE</td>
+						<td>AMOUNT AMOUNT</td>
+						<td>AMOUNT AMOUNT</td>
+					</tr>
+					<tr>
+						<td>DATE DATE</td>
+						<td>AMOUNT AMOUNT</td>
+						<td>AMOUNT AMOUNT</td>
+					</tr>
+					<tr>
+						<td>DATE DATE</td>
+						<td>AMOUNT AMOUNT</td>
+						<td>AMOUNT AMOUNT</td>
+					</tr>
+					<tr>
+						<td>DATE DATE</td>
+						<td>AMOUNT AMOUNT</td>
+						<td>AMOUNT AMOUNT</td>
+					</tr>
+					<tr>
+						<td>DATE DATE</td>
+						<td>AMOUNT AMOUNT</td>
+						<td>AMOUNT AMOUNT</td>
+					</tr>
+					<tr>
+						<td>DATE DATE</td>
+						<td>AMOUNT AMOUNT</td>
+						<td>AMOUNT AMOUNT</td>
 					</tr>
 					<tr>
 						<td>DATE DATE</td>
