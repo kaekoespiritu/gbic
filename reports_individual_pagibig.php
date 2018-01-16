c55bee5aa90c0f390a4b87a02bb3444e04fc2b3b
JustineDiza <justinediza@gmail.com>
2018-01-17 02:42:13 +0800

    Added breadcrumb and back button for pagibig, philhealth and sss individual contributions page

--- reports_individual_pagibig.php
+++ reports_individual_pagibig.php
@@ -22,191 +22,200 @@
 		require_once("directives/nav.php");
 		?>
 
-		<h3 class="pull-down">PagIBIG Contributions Report for [NAME], [POSITION] at [SITE]</h3>
-
-		<div class="col-md-4">
-			<div class="pull-down">
-			<button class="btn btn-default">
-				Print Weekly
-			</button>
-			<table class="table table-bordered pull-down">
-				<tr>
-					<td colspan="4">
-						[NAME], [POSITION] at [SITE] 
-					</td>
-				</tr>
-				<tr>
-					<td rowspan="2">
-						Week
-					</td>
-					<td colspan="2">
-						PagIBIG
-					</td>
-					<td rowspan="2">
-						Total
-					</td>
-				</tr>
-				<tr>
-					<td>
-						Employee
-					</td>
-					<td>
-						Employer
-					</td>
-				</tr>
-				<tr>
-					<td>
-						Dec. 1-7
-					</td>
-					<td>
-						$$$
-					</td>
-					<td>
-						$$$
-					</td>
-					<td>
-						$$$
-					</td>
-				</tr>
-				<tr>
-					<td>
-						
-					</td>
-					<td>
-						
-					</td>
-					<td>
-						Grand Total
-					</td>
-					<td>
-						$$$
-					</td>
-				</tr>
-			</table>
+		<div class="col-md-10 col-md-offset-1">
+			<div class="row"><br>
+				<div class="row text-center">
+					<ol class="breadcrumb text-left">
+						<li><a href='reports_individual_contributions.php?type=Contributions&period=week&site=null&position=null' class="btn btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Contributions</a></li>
+						<li>Individual PagIBIG Contributions Report for [NAME], [POSITION] at [SITE]</li>
+					</ol>
+				</div>
 			</div>
-		</div>
 
-		<div class="col-md-4">
-			<div class="pull-down">
-			<button class="btn btn-default">
-				Print Monthly
-			</button>
-			<table class="table table-bordered pull-down">
-				<tr>
-					<td colspan="4">
-						[NAME], [POSITION] at [SITE] 
-					</td>
-				</tr>
-				<tr>
-					<td rowspan="2">
-						Month
-					</td>
-					<td colspan="2">
-						PagIBIG
-					</td>
-					<td rowspan="2">
-						Total
-					</td>
-				</tr>
-				<tr>
-					<td>
-						Employee
-					</td>
-					<td>
-						Employer
-					</td>
-				</tr>
-				<tr>
-					<td>
-						December
-					</td>
-					<td>
-						$$$
-					</td>
-					<td>
-						$$$
-					</td>
-					<td>
-						$$$
-					</td>
-				</tr>
-				<tr>
-					<td>
-						
-					</td>
-					<td>
-						
-					</td>
-					<td>
-						Grand Total
-					</td>
-					<td>
-						$$$
-					</td>
-				</tr>
-			</table>
+			<div class="col-md-4">
+				<div class="pull-down">
+				<button class="btn btn-default">
+					Print Weekly
+				</button>
+				<table class="table table-bordered pull-down">
+					<tr>
+						<td colspan="4">
+							[NAME], [POSITION] at [SITE] 
+						</td>
+					</tr>
+					<tr>
+						<td rowspan="2">
+							Week
+						</td>
+						<td colspan="2">
+							PagIBIG
+						</td>
+						<td rowspan="2">
+							Total
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Employee
+						</td>
+						<td>
+							Employer
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Dec. 1-7
+						</td>
+						<td>
+							$$$
+						</td>
+						<td>
+							$$$
+						</td>
+						<td>
+							$$$
+						</td>
+					</tr>
+					<tr>
+						<td>
+							
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							Grand Total
+						</td>
+						<td>
+							$$$
+						</td>
+					</tr>
+				</table>
+				</div>
 			</div>
-		</div>
 
-		<div class="col-md-4">
-			<div class="pull-down">
-			<button class="btn btn-default">
-				Print Yearly
-			</button>
-			<table class="table table-bordered pull-down">
-				<tr>
-					<td colspan="4">
-						[NAME], [POSITION] at [SITE] 
-					</td>
-				</tr>
-				<tr>
-					<td rowspan="2">
-						Year
-					</td>
-					<td colspan="2">
-						PagIBIG
-					</td>
-					<td rowspan="2">
-						Total
-					</td>
-				</tr>
-				<tr>
-					<td>
-						Employee
-					</td>
-					<td>
-						Employer
-					</td>
-				</tr>
-				<tr>
-					<td>
-						2017
-					</td>
-					<td>
-						$$$
-					</td>
-					<td>
-						$$$
-					</td>
-					<td>
-						$$$
-					</td>
-				</tr>
-				<tr>
-					<td>
-						
-					</td>
-					<td>
-						
-					</td>
-					<td>
-						Grand Total
-					</td>
-					<td>
-						$$$
-					</td>
-				</tr>
-			</table>
+			<div class="col-md-4">
+				<div class="pull-down">
+				<button class="btn btn-default">
+					Print Monthly
+				</button>
+				<table class="table table-bordered pull-down">
+					<tr>
+						<td colspan="4">
+							[NAME], [POSITION] at [SITE] 
+						</td>
+					</tr>
+					<tr>
+						<td rowspan="2">
+							Month
+						</td>
+						<td colspan="2">
+							PagIBIG
+						</td>
+						<td rowspan="2">
+							Total
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Employee
+						</td>
+						<td>
+							Employer
+						</td>
+					</tr>
+					<tr>
+						<td>
+							December
+						</td>
+						<td>
+							$$$
+						</td>
+						<td>
+							$$$
+						</td>
+						<td>
+							$$$
+						</td>
+					</tr>
+					<tr>
+						<td>
+							
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							Grand Total
+						</td>
+						<td>
+							$$$
+						</td>
+					</tr>
+				</table>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="pull-down">
+				<button class="btn btn-default">
+					Print Yearly
+				</button>
+				<table class="table table-bordered pull-down">
+					<tr>
+						<td colspan="4">
+							[NAME], [POSITION] at [SITE] 
+						</td>
+					</tr>
+					<tr>
+						<td rowspan="2">
+							Year
+						</td>
+						<td colspan="2">
+							PagIBIG
+						</td>
+						<td rowspan="2">
+							Total
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Employee
+						</td>
+						<td>
+							Employer
+						</td>
+					</tr>
+					<tr>
+						<td>
+							2017
+						</td>
+						<td>
+							$$$
+						</td>
+						<td>
+							$$$
+						</td>
+						<td>
+							$$$
+						</td>
+					</tr>
+					<tr>
+						<td>
+							
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							Grand Total
+						</td>
+						<td>
+							$$$
+						</td>
+					</tr>
+				</table>
+				</div>
 			</div>
 		</div>
 
