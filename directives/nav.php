7376ef4e4ed4f1b141cda782ed921f48c9c3e6f1
JustineDiza <justinediza@gmail.com>
2018-01-19 11:37:48 +0800

    Modified link for payslip pages

--- directives/nav.php
+++ directives/nav.php
@@ -79,7 +79,7 @@
 			<div class="sub-flipdown">
 				<div class="sub-flipdown-menu">
 					<a href="reports_payslip_with.php?type=Payslip&period=week&site=null&position=null"> With Requirements</a>
-					<a href="reports_payslip_without.php?type=Payslip&period=Weekly"> Without Requirements</a>
+					<a href="reports_payslip_without.php?type=Payslip&period=week&site=null&position=null"> Without Requirements</a>
 				</div>
 				<a class="subflipbtn">Payslip</a>
 			</div>
