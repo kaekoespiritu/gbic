fd92929fbc64c92f70a35e428eab1c90a33a1a85
JustineDiza <justinediza@gmail.com>
2018-01-25 12:15:01 +0800

    Added overflow on Payroll table

--- reports_overall_payroll.php
+++ reports_overall_payroll.php
@@ -108,273 +108,275 @@
 			</div>
     	</div>
 
-	
-		<?php 
 
-			if(isset($_POST['payrollDate'])) {
-				Print '<div class="row pull-down">
-							<a class="btn btn-default" href="print_payroll.php?site='.$location.'&date='.$_POST['payrollDate'].'">
-								Print Payroll
-							</a>
-						</div>';
-				if($req == 'all')
-					$reqMessage = "All ".$location." employees";
-				else if($req == 'withReq')
-					$reqMessage = $location." W/ Requirements";
-				else if($req == 'withOReq')
-					$reqMessage = $location." W/o Requirements";
-				Print '
-						<table class="table table-bordered pull-down">
-							<tr>
-								<td colspan="6">
-									'.$reqMessage .'
-								</td>
-								<td colspan="21" rowspan="2" class="vertical-align">
-									PAYROLL
-								</td>
-							</tr>
-							<tr>
-								<td colspan="6">
-									Date covered: '.$startDate.' - '.$_POST['payrollDate'].'
-								</td>
-							</tr>
-							<tr>
-								<td>
-									#
-								</td>
-								<td>
-									Name
-								</td>
-								<td>
-									Position
-								</td>
-								<td>
-									Rate
-								</td>
-								<td>
-									# of days
-								</td>
-								<td>
-									O.T.
-								</td>
-								<td>
-									# of hours
-								</td>
-								<td>
-									Allow.
-								</td>
-								<td>
-									COLA
-								</td>
-								<td>
-									Sun
-								</td>
-								<td>
-									D
-								</td>
-								<td>
-									hrs
-								</td>
-								<td>
-									N.D.
-								</td>
-								<td>
-									#
-								</td>
-								<td>
-									Reg. Hol
-								</td>
-								<td>
-									#
-								</td>
-								<td>
-									Spe. Hol
-								</td>
-								<td>
-									#
-								</td>
-								<td>
-									X All.
-								</td>
-								<td>
-									SSS
-								</td>
-								<td>
-									Philhealth
-								</td>
-								<td>
-									PagIBIG
-								</td>
-								<td>
-									Old vale
-								</td>
-								<td>
-									vale
-								</td>
-								<td>
-									tools
-								</td>
-								<td>
-									Total Salary
-								</td>
-							</tr>';
-						
-						$date = $_POST['payrollDate'];
+			<?php 
 
-						if($req == 'all')
-						{
-							$employee = "SELECT * FROM employee WHERE site = '$location' ORDER BY lastname ASC, position ASC";
-						}
-						else if($req == 'withReq')
-						{
-							$employee = "SELECT * FROM employee WHERE site = '$location' AND complete_doc = '1'ORDER BY lastname ASC, position ASC";
-						}
-						else if($req == 'withOReq')
-						{
-							$employee = "SELECT * FROM employee WHERE site = '$location' AND complete_doc = '0'ORDER BY lastname ASC, position ASC";
-						}
+				if(isset($_POST['payrollDate'])) {
+					Print '<div class="row pull-down">
+								<a class="btn btn-default" href="print_payroll.php?site='.$location.'&date='.$_POST['payrollDate'].'">
+									Print Payroll
+								</a>
+							</div>';
+					if($req == 'all')
+						$reqMessage = "All ".$location." employees";
+					else if($req == 'withReq')
+						$reqMessage = $location." W/ Requirements";
+					else if($req == 'withOReq')
+						$reqMessage = $location." W/o Requirements";
+					Print '<div class="row">
+							<div class="col-md-12 overflow">
+							<table class="table table-bordered pull-down">
+								<tr>
+									<td colspan="6">
+										'.$reqMessage .'
+									</td>
+									<td colspan="21" rowspan="2" class="vertical-align">
+										PAYROLL
+									</td>
+								</tr>
+								<tr>
+									<td colspan="6">
+										Date covered: '.$startDate.' - '.$_POST['payrollDate'].'
+									</td>
+								</tr>
+								<tr>
+									<td>
+										#
+									</td>
+									<td>
+										Name
+									</td>
+									<td>
+										Position
+									</td>
+									<td>
+										Rate
+									</td>
+									<td>
+										# of days
+									</td>
+									<td>
+										O.T.
+									</td>
+									<td>
+										# of hours
+									</td>
+									<td>
+										Allow.
+									</td>
+									<td>
+										COLA
+									</td>
+									<td>
+										Sun
+									</td>
+									<td>
+										D
+									</td>
+									<td>
+										hrs
+									</td>
+									<td>
+										N.D.
+									</td>
+									<td>
+										#
+									</td>
+									<td>
+										Reg. Hol
+									</td>
+									<td>
+										#
+									</td>
+									<td>
+										Spe. Hol
+									</td>
+									<td>
+										#
+									</td>
+									<td>
+										X All.
+									</td>
+									<td>
+										SSS
+									</td>
+									<td>
+										Philhealth
+									</td>
+									<td>
+										PagIBIG
+									</td>
+									<td>
+										Old vale
+									</td>
+									<td>
+										vale
+									</td>
+									<td>
+										tools
+									</td>
+									<td>
+										Total Salary
+									</td>
+								</tr>';
+							
+							$date = $_POST['payrollDate'];
 
-					
-					$dataBool = true;//Boolean if there is data for the payroll
-					$employeeQuery = mysql_query($employee);
-					if(mysql_num_rows($employeeQuery) >= 1)
-					{
-						Print "<script>console.log('1')</script>";
-						$color = "#ECF0F1";//for alternating color
-						$rowNum = 1;
-						
-						while($employeeArr = mysql_fetch_assoc($employeeQuery))
-						{
-							Print "<script>console.log('2')</script>";
-							$emplid = $employeeArr['empid'];
-							$payroll = "SELECT * FROM payroll WHERE date = '$date' AND empid='$emplid'";
-							$payrollQuery = mysql_query($payroll);
-							if(mysql_num_rows($payrollQuery) >= 1)
+							if($req == 'all')
 							{
-								$dataBool = false;
-								Print "<script>console.log('3')</script>";
-								$payrollArr = mysql_fetch_assoc($payrollQuery);
-
-									$color = ($rowNum % 2 == 0 ? "#ECF0F1" : "#FDFEFE");//alternating color
-
-									Print '	<tr bgcolor="'.$color.'">
-												<td><!-- # -->
-													'.$rowNum.'
-												</td>
-												<td><!-- Name -->
-													'.$employeeArr['lastname'].', '.$employeeArr['firstname'].'
-												</td>
-												<td><!-- Position -->
-													'.$employeeArr['position'].'
-												</td>
-												<td><!-- Rate -->
-													'.$payrollArr['rate'].'
-												</td>
-												<td><!-- # of days -->
-													'.$payrollArr['num_days'].'
-												</td>
-												<td><!-- OT -->
-													'.$payrollArr['overtime'].'
-												</td>
-												<td><!-- # of hours -->
-													'.$payrollArr['ot_num'].'
-												</td>
-												<td><!-- Allow -->
-													'.$payrollArr['allow'].'
-												</td>
-												<td><!-- COLA -->
-													'.$payrollArr['cola'].'
-												</td>
-												<td><!-- Sun -->
-													'.$payrollArr['sunday_rate'].'
-												</td>
-												<td><!-- D -->
-													'.$payrollArr['sunday_att'].'
-												</td>
-												<td><!-- hrs -->
-													'.$payrollArr['sunday_hrs'].'
-												</td>
-												<td><!-- ND -->
-													'.$payrollArr['nightdiff_rate'].'
-												</td>
-												<td><!-- # -->
-													'.$payrollArr['nightdiff_num'].'
-												</td>
-												<td><!-- Reg.hol -->
-													'.$payrollArr['reg_holiday'].'
-												</td>
-												<td><!-- # -->
-													'.$payrollArr['reg_holiday_num'].'
-												</td>
-												<td><!-- Spe. hol -->
-													'.$payrollArr['spe_holiday'].'
-												</td>
-												<td><!-- # -->
-													'.$payrollArr['spe_holiday_num'].'
-												</td>
-												<td><!-- X.All -->
-													'.$payrollArr['x_allowance'].'
-												</td>
-												<td><!-- SSS -->
-													'.$payrollArr['sss'].'
-												</td>
-												<td><!-- Philhealth -->
-													'.$payrollArr['philhealth'].'
-												</td>
-												<td><!-- Pagibig -->
-													'.$payrollArr['pagibig'].'
-												</td>
-												<td><!-- Old vale -->
-													'.$payrollArr['old_vale'].'
-												</td>
-												<td><!-- vale -->
-													'.$payrollArr['new_vale'].'
-												</td>
-												<td><!-- tools -->
-													'.$payrollArr['tools_paid'].'
-												</td>
-												<td><!-- Total Salary -->
-													'.numberExactFormat($payrollArr['total_salary'],2,".").'
-												</td>
-											</tr>';
-										
-										$rowNum++;//increment the row number
+								$employee = "SELECT * FROM employee WHERE site = '$location' ORDER BY lastname ASC, position ASC";
 							}
+							else if($req == 'withReq')
+							{
+								$employee = "SELECT * FROM employee WHERE site = '$location' AND complete_doc = '1'ORDER BY lastname ASC, position ASC";
+							}
+							else if($req == 'withOReq')
+							{
+								$employee = "SELECT * FROM employee WHERE site = '$location' AND complete_doc = '0'ORDER BY lastname ASC, position ASC";
+							}
+
+						
+						$dataBool = true;//Boolean if there is data for the payroll
+						$employeeQuery = mysql_query($employee);
+						if(mysql_num_rows($employeeQuery) >= 1)
+						{
+							Print "<script>console.log('1')</script>";
+							$color = "#ECF0F1";//for alternating color
+							$rowNum = 1;
+							
+							while($employeeArr = mysql_fetch_assoc($employeeQuery))
+							{
+								Print "<script>console.log('2')</script>";
+								$emplid = $employeeArr['empid'];
+								$payroll = "SELECT * FROM payroll WHERE date = '$date' AND empid='$emplid'";
+								$payrollQuery = mysql_query($payroll);
+								if(mysql_num_rows($payrollQuery) >= 1)
+								{
+									$dataBool = false;
+									Print "<script>console.log('3')</script>";
+									$payrollArr = mysql_fetch_assoc($payrollQuery);
+
+										$color = ($rowNum % 2 == 0 ? "#ECF0F1" : "#FDFEFE");//alternating color
+
+										Print '	<tr bgcolor="'.$color.'">
+													<td><!-- # -->
+														'.$rowNum.'
+													</td>
+													<td><!-- Name -->
+														'.$employeeArr['lastname'].', '.$employeeArr['firstname'].'
+													</td>
+													<td><!-- Position -->
+														'.$employeeArr['position'].'
+													</td>
+													<td><!-- Rate -->
+														'.$payrollArr['rate'].'
+													</td>
+													<td><!-- # of days -->
+														'.$payrollArr['num_days'].'
+													</td>
+													<td><!-- OT -->
+														'.$payrollArr['overtime'].'
+													</td>
+													<td><!-- # of hours -->
+														'.$payrollArr['ot_num'].'
+													</td>
+													<td><!-- Allow -->
+														'.$payrollArr['allow'].'
+													</td>
+													<td><!-- COLA -->
+														'.$payrollArr['cola'].'
+													</td>
+													<td><!-- Sun -->
+														'.$payrollArr['sunday_rate'].'
+													</td>
+													<td><!-- D -->
+														'.$payrollArr['sunday_att'].'
+													</td>
+													<td><!-- hrs -->
+														'.$payrollArr['sunday_hrs'].'
+													</td>
+													<td><!-- ND -->
+														'.$payrollArr['nightdiff_rate'].'
+													</td>
+													<td><!-- # -->
+														'.$payrollArr['nightdiff_num'].'
+													</td>
+													<td><!-- Reg.hol -->
+														'.$payrollArr['reg_holiday'].'
+													</td>
+													<td><!-- # -->
+														'.$payrollArr['reg_holiday_num'].'
+													</td>
+													<td><!-- Spe. hol -->
+														'.$payrollArr['spe_holiday'].'
+													</td>
+													<td><!-- # -->
+														'.$payrollArr['spe_holiday_num'].'
+													</td>
+													<td><!-- X.All -->
+														'.$payrollArr['x_allowance'].'
+													</td>
+													<td><!-- SSS -->
+														'.$payrollArr['sss'].'
+													</td>
+													<td><!-- Philhealth -->
+														'.$payrollArr['philhealth'].'
+													</td>
+													<td><!-- Pagibig -->
+														'.$payrollArr['pagibig'].'
+													</td>
+													<td><!-- Old vale -->
+														'.$payrollArr['old_vale'].'
+													</td>
+													<td><!-- vale -->
+														'.$payrollArr['new_vale'].'
+													</td>
+													<td><!-- tools -->
+														'.$payrollArr['tools_paid'].'
+													</td>
+													<td><!-- Total Salary -->
+														'.numberExactFormat($payrollArr['total_salary'],2,".").'
+													</td>
+												</tr>';
+											
+											$rowNum++;//increment the row number
+								}
+									
+							}
+
+
 								
 						}
-
-
 							
-					}
 						
-					
-					else
-					{
-						$dataBool = false;
-						Print '	<tr>
-									<td colspan="27">
-										<h4>No Payroll data at the moment</h4>
-									</td>
-								</tr>';
-					}
+						else
+						{
+							$dataBool = false;
+							Print '	<tr>
+										<td colspan="27">
+											<h4>No Payroll data at the moment</h4>
+										</td>
+									</tr>';
+						}
 
-					if($dataBool)
-					{
-						Print '	<tr>
-									<td colspan="27">
-										<h4>No Payroll data at the moment</h4>
-									</td>
-								</tr>';
-					}
-					Print '</table>';
-					
-		}
-		else
-		{
-			Print "<h4 class='pull-down-more'>Select Requirements and Date to view Payroll report.</h4>";
-		}
-		?>
-		
+						if($dataBool)
+						{
+							Print '	<tr>
+										<td colspan="27">
+											<h4>No Payroll data at the moment</h4>
+										</td>
+									</tr>';
+						}
+						Print '</table>';
+						
+			}
+			else
+			{
+				Print "<h4 class='pull-down-more'>Select Requirements and Date to view Payroll report.</h4>";
+			}
+			?>
+			</div>
+		</div>
 
 	</div>
 </div>
