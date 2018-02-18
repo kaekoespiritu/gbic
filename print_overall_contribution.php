5646e7927a0e7c63ed4e02edad7becdf4f750e0b
JustineDiza <justinediza@gmail.com>
2018-02-19 02:45:24 +0800

    Fed datverall contributions but fatal error occurs, needs a fix

--- print_overall_contribution.php
+++ print_overall_contribution.php
@@ -4,56 +4,140 @@ include_once('directives/db.php');
 include_once 'modules/Classes/PHPExcel.php';
 include('directives/print_styles.php'); //Styles for PHPexcel
 
+$site = $_GET['site'];
+$period = $_GET['period'];
+$contributionType = $_GET['contribution'];
 
-$filename = "Overall SSS Contributions.xls";
-// Filename: Last name, First name SSS contribution for Date
-
-// Variables to get:
-// Period
-// Contribution Type
-// Employee ID
+$filename = "Overall ".$contributionType." Contributions for ".$site.".xls";
+// Filename: Overall SSS contributions for Site
 
 $sheet = new PHPExcel();
 
 $activeSheet = $sheet -> createSheet(0);
 
-// Sheets named by site
+// * ======= Styling table ======= * //
+	// Merge cells
+	$activeSheet->mergeCells('A1:F1'); // Header
+	$activeSheet->mergeCells('A2:A3'); // Period
+	$activeSheet->mergeCells('B2:B3'); // Name
+	$activeSheet->mergeCells('C2:C3'); // Position
+	$activeSheet->mergeCells('D2:E2'); // Contribution Header
+	$activeSheet->mergeCells('F2:F3'); // Total
 
-// Merge cells
-$activeSheet->mergeCells('A1:F1'); // Header
-$activeSheet->mergeCells('A2:A3'); // Period
-$activeSheet->mergeCells('B2:B3'); // Name
-$activeSheet->mergeCells('C2:C3'); // Position
-$activeSheet->mergeCells('D2:E2'); // Contribution Header
-$activeSheet->mergeCells('F2:F3'); // Total
+	// Fill in Header text
+	$activeSheet->setCellValue('A1', 'Overall Site Contribution	');
+	$activeSheet->setCellValue('A2', $period);
+	$activeSheet->setCellValue('B2', 'Name');
+	$activeSheet->setCellValue('C2', 'Position');
+	$activeSheet->setCellValue('D2', $contributionType);
+	$activeSheet->setCellValue('F2', 'Total');
+	$activeSheet->setCellValue('D3', 'Employee');
+	$activeSheet->setCellValue('E3', 'Employer');
 
-// Header
-$activeSheet->setCellValue('A1', 'Overall Site Contribution	');
-$activeSheet->setCellValue('A2', 'Period Type');
-$activeSheet->setCellValue('B2', 'Name');
-$activeSheet->setCellValue('C2', 'Position');
-$activeSheet->setCellValue('D2', 'SSS'); // Contribution type
-$activeSheet->setCellValue('F2', 'Total');
-$activeSheet->setCellValue('D3', 'Employee');
-$activeSheet->setCellValue('E3', 'Employer');
+	// Centering text
+	$activeSheet->getStyle('A1')->applyFromArray($align_center); // Centered header text
+	$activeSheet->getStyle('A2')->applyFromArray($align_center); // Centered Period type text
+	$activeSheet->getStyle('B2')->applyFromArray($align_center); // Centered name text
+	$activeSheet->getStyle('C2')->applyFromArray($align_center); // Centered position text
+	$activeSheet->getStyle('D2')->applyFromArray($align_center); // Centered contribution type text
+	$activeSheet->getStyle('F2')->applyFromArray($align_center); // Centered contribution type text
+	
+	// Changing cell width
+	$activeSheet->getColumnDimension('A')->setAutoSize(true); // Lengthen Period
+	$activeSheet->getColumnDimension('B')->setAutoSize(true); // Lengthen Name
+	$activeSheet->getColumnDimension('C')->setAutoSize(true); // Lengthen Position
+	
+	// Styling borders
+	$activeSheet->getStyle('A:F')->applyFromArray($border_all_thin);
 
-// Style
-// --- Centering text
-$activeSheet->getStyle('A1')->applyFromArray($align_center); // Centered header text
-$activeSheet->getStyle('A2')->applyFromArray($align_center); // Centered Period type text
-$activeSheet->getStyle('B2')->applyFromArray($align_center); // Centered name text
-$activeSheet->getStyle('C2')->applyFromArray($align_center); // Centered position text
-$activeSheet->getStyle('D2')->applyFromArray($align_center); // Centered contribution type text
-$activeSheet->getStyle('F2')->applyFromArray($align_center); // Centered contribution type text
-// -- Changing cell width
-$activeSheet->getColumnDimension('A')->setAutoSize(true); // Lengthen Period
-$activeSheet->getColumnDimension('B')->setAutoSize(true); // Lengthen Name
-$activeSheet->getColumnDimension('C')->setAutoSize(true); // Lengthen Position
-// --- Styling borders
-$activeSheet->getStyle('A1:F3')->applyFromArray($border_all_thin);
+// * ======= Data Feeding ======= * //
+// Get contribution details for selected site
+$contributionQuery = " AND payroll.".strtolower($contributionType)." != 0 ";
+$employeelist = "
+	SELECT
+		employee.empid,
+		employee.lastname,
+		employee.firstname,
+		employee.position,
+		payroll.date,
+		payroll.sss,
+		payroll.sss_er,
+		payroll.philhealth,
+		payroll.philhealth_er,
+		payroll.pagibig,
+		payroll.pagibig_er
+	FROM `payroll` INNER JOIN employee ON payroll.empid=employee.empid
+	WHERE employee.site = '$site' $contributionQuery
+	ORDER BY payroll.date DESC, employee.empid";
 
+$employeelistQuery = mysql_query($employeelist) or die (mysql_error());
 
-//----------------- Body ---------------------//
+$rowCounter = 4; // Start for the data in the row of excel
+
+$GrandTotalSSS = $GrandTotalPhilHealth = $GrandTotalPagIBIG = $totalEmployeeSSS = $totalEmployerSSS = $totalEmployeePhilHealth = $totalEmployerPhilHealth = $totalEmployeePagIBIG = $totalEmployerPagIBIG = 0;
+
+$activeSheet->setCellValue('A1', 'Overall '.$contributionType.' Contribution at '.$site);
+
+while($employeelistArr = mysql_fetch_assoc($employeelistQuery)){
+
+	$endDate = $employeelistArr['date'];
+	$startDate = date('F j, Y', strtotime('-6 day', strtotime($endDate)));
+
+	$name = $employeelistArr['lastname'] . ", " . $employeelistArr['firstname'];
+
+	$activeSheet->setCellValue('A'.$rowCounter, $startDate.' - '.$endDate); // Period
+
+	$activeSheet->setCellValue('B'.$rowCounter, $name); // Name
+	$activeSheet->setCellValue('C'.$rowCounter, $employeelistArr['position']); // Position
+
+	switch($contributionType){
+		case 'SSS':
+			$activeSheet->setCellValue('D'.$rowCounter, $employeelistArr['sss']); // Employee
+			$activeSheet->setCellValue('E'.$rowCounter, $employeelistArr['sss_er']); // Employer
+			$activeSheet->setCellValue('F'.$rowCounter, $employeelistArr['sss'] + $employeelistArr['sss_er']); // Total
+			$totalEmployeeSSS += $employeelistArr['sss'];
+			$totalEmployerSSS += $employeelistArr['sss_er'];
+		break;
+		case 'PhilHealth':
+			$activeSheet->setCellValue('D'.$rowCounter, $employeelistArr['philhealth']); // Employee
+			$activeSheet->setCellValue('E'.$rowCounter, $employeelistArr['philhealth_er']); // Employer
+			$activeSheet->setCellValue('F'.$rowCounter, $employeelistArr['philhealth'] + $employeelistArr['philhealth_er']); // Total
+			$totalEmployeePhilHealth += $employeelistArr['philhealth'];
+			$totalEmployerPhilHealth += $employeelistArr['philhealth_er'];
+		break;
+		case 'PagIbig':
+			$activeSheet->setCellValue('D'.$rowCounter, $employeelistArr['pagibig']); // Employee
+			$activeSheet->setCellValue('E'.$rowCounter, $employeelistArr['pagibig_er']); // Employer
+			$activeSheet->setCellValue('F'.$rowCounter, $employeelistArr['pagibig'] + $employeelistArr['pagibig_er']); // Total
+			$totalEmployeePagIbig += $employeelistArr['pagibig'];
+			$totalEmployerPagIbig += $employeelistArr['pagibig_er'];
+		break;
+		case 'Overall':
+
+		break;
+	}
+
+	$rowCounter++;
+
+}
+
+switch($contributionType){
+	case 'SSS':
+		$GrandTotal = $totalEmployeeSSS + $totalEmployerSSS;
+	break;
+	case 'PhilHealth':
+		$GrandTotal = $totalEmployeePhilHealth + $totalEmployerPhilHealth;
+	break;
+	case 'PagIbig':
+		$GrandTotal = $totalEmployeePagIBIG + $totalEmployerPagIBIG;
+	break;
+}
+
+$activeSheet->mergeCells('A'.$rowCounter.':E'.$rowCounter);
+$activeSheet->setCellValue('A'.$rowCounter, 'Grand Total');
+$activeSheet->setCellValue('F'.$rowCounter, $GrandTotal); // Total
+$activeSheet->getStyle('A1:F'.$rowCounter)->applyFromArray($border_all_thin); 
+$activeSheet->getStyle('A'.$rowCounter)->applyFromArray($align_right); // Centered header text
 
 
 header('Content-Type: application/vnd.ms-excel');
