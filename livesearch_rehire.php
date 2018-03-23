89b6ce407f3282fdcbad2ac4feb6e04b393d783e
JustineDiza <justinediza@gmail.com>
2018-03-23 14:43:57 +0800

    Fixed alignment of rehire employee modal

--- livesearch_rehire.php
+++ livesearch_rehire.php
@@ -31,7 +31,7 @@ if(!empty($result))
       {
     
       $output .= '
-      <div class="col-md-6 col-md-offset-3 searchResults text-left" id="'.$row['empid'].'">
+      <div class="col-md-6 col-md-offset-3 searchresults text-left" id="'.$row['empid'].'">
       <a href="#"  onclick="sendToModal(\''.$row['empid'].'\')" data-toggle="modal" data-target="#oldEmployee">'.$row['lastname'].', '.$row['firstname'].'<cite>['.$row['position'].', '.$row['site'].']</cite></a>
       <input type="hidden" class="toModalEmpid" value="'.$row['empid'].'">
       <input type="hidden" class="toModalAddress" value="'.$row['address'].'">
@@ -60,7 +60,7 @@ if(!empty($result))
 }
 else
 {   
-   echo '<div class="col-md-6 col-md-offset-3 searchResults text-left">
+   echo '<div class="col-md-6 col-md-offset-3 searchresults text-left">
    Employee Not Found...
    </div>';
 }
