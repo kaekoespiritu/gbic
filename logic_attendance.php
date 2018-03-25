82b57ebea4be21985dcef5bf8dbb732eaa7e04ee
Karlo Espiritu <karlo.espiritu@yahoo.com>
2018-03-25 09:39:25 +0800

    fixed condition calsulation in attendance logic

--- logic_attendance.php
+++ logic_attendance.php
@@ -156,7 +156,7 @@ if(!empty($dateRows))// Updating attendance
 			$AttQuery .= ",";
 		}
 		if(((!empty($_POST['timein1'][$counter]) && !empty($_POST['timeout1'][$counter])) && 
-			(!empty($_POST['timein2'][$counter]) && !empty($_POST['timeout2'][$counter]))) || (empty($_POST['timein2'][$counter] && empty($_POST['timeout2'][$counter]))) && $_POST['attendance'][$counter] == "PRESENT")
+			(!empty($_POST['timein2'][$counter]) && !empty($_POST['timeout2'][$counter]))) || ((empty($_POST['timein2'][$counter] && empty($_POST['timeout2'][$counter]))) && $_POST['attendance'][$counter] == "PRESENT"))
 		{	
 			Print "<script>console.log('present')</script>";
 			$empid = $_POST['empid'][$counter];
