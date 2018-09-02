c49005eac044060dbaeb5400faa2b950b875b1b2
JustineDiza <justinediza@gmail.com>
2018-09-02 22:50:23 +0800

    Updated list of colors for bank

--- directives/modals/addBank.php
+++ directives/modals/addBank.php
@@ -12,9 +12,16 @@
 				     		<input type="text" class="form-control" name="bank_name" placeholder="Name of new bank">
 				     		<div align="left">
 				     			<h3 align="left">Color:</h3>
-				     			<input type="radio" value="0074D9" name="color[]"> <span class="swatch_1"></span> <span class="color_font">Blue</span><br>
-				     			<input type="radio" value="2ECC40" name="color[]"> <span class="swatch_2"></span> <span class="color_font">Green</span><br>
-				     			<input type="radio" value="FF851B" name="color[]"> <span class="swatch_3"></span> <span class="color_font">Orange</span>
+				     			<input type="radio" value="94D0EA" name="color[]"> <span class="swatch_1"></span> <span class="color_font">Light Cornflower Blue</span><br>
+				     			<input type="radio" value="FAEE8A" name="color[]"> <span class="swatch_2"></span> <span class="color_font">Flavescent</span><br>
+				     			<input type="radio" value="A6C26D" name="color[]"> <span class="swatch_3"></span> <span class="color_font">Middle Green Yellow</span><br>
+				     			<input type="radio" value="E3866E" name="color[]"> <span class="swatch_4"></span> <span class="color_font">Middle Red</span><br>
+				     			<input type="radio" value="F79A5E" name="color[]"> <span class="swatch_5"></span> <span class="color_font">Sandy Brown</span><br>
+				     			<input type="radio" value="FE938C" name="color[]"> <span class="swatch_6"></span> <span class="color_font">Tulip</span><br>
+				     			<input type="radio" value="E6B89C" name="color[]"> <span class="swatch_7"></span> <span class="color_font">Pale Gold</span><br>
+				     			<input type="radio" value="EAD2AC" name="color[]"> <span class="swatch_8"></span> <span class="color_font">Desert Sand</span><br>
+				     			<input type="radio" value="9CAFB7" name="color[]"> <span class="swatch_9"></span> <span class="color_font">Pewter Blue</span><br>
+				     			<input type="radio" value="4281A4" name="color[]"> <span class="swatch_10"></span> <span class="color_font">Steel Blue</span>
 				     		</div>
 				     	</div>
 				     	<div class="modal-footer">
