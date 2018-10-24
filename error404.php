6ed6751be291048e1c746af2f7dd95b500f65446
JustineDiza <justinediza@gmail.com>
2018-10-24 13:56:48 +0800

    protocol

---
+++ error404.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>Payroll</title>
+	<!-- Company Name: Green Built Industrial Corporation -->
+
+	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+
+	<style>
+	body
+	{
+		background: #fafafa;
+	  	text-align: center;
+	  	height:100% !important;
+	}
+
+	</style>
+
+</head>
+<body>
+
+	<h1 class="pull-down">Error encountered.</h1>
+
+	<h2>Free trial has ended. License is needed to proceed. Please contact the developers.</h2>
+</body>
+</html>
