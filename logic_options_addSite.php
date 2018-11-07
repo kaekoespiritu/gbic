17cdb55124395fc2ec2b85fdd58f06eec32bab49
Karlo Espiritu <karlo.espiritu@yahoo.com>
2018-11-07 15:04:02 +0800

    Resolved Issue #43

--- logic_options_addSite.php
+++ logic_options_addSite.php
@@ -7,21 +7,21 @@
 	$siteName = ucwords($siteName);
 	$start = strftime("%B %d, %Y");
 	
-	$siteChecker = "SELECT * FROM site WHERE location = '$siteName'";
-	$checkerQuery = mysql_query($siteChecker);
+	// $siteChecker = "SELECT * FROM site WHERE location = '$siteName'";
+	// $checkerQuery = mysql_query($siteChecker);
 
-	if(mysql_num_rows($checkerQuery) == 0)//Check if site name is already in the database
-	{
+	// if(mysql_num_rows($checkerQuery) == 0)//Check if site name is already in the database
+	// {
 		$site = "INSERT INTO site(location, active, start) VALUES('$siteName','1', '$start')";
 		$siteQuery = mysql_query($site);
 		Print "<script>alert('Successfully added ".$siteName." from active Sites')</script>";
 		Print "<script>window.location.assign('options.php')</script>";
-	}
-	else
-	{
-		Print "<script>alert('Site name already exist.')</script>";
-		Print "<script>window.location.assign('options.php')</script>";
-	}
+	// }
+	// else
+	// {
+	// 	Print "<script>alert('Site name already exist.')</script>";
+	// 	Print "<script>window.location.assign('options.php')</script>";
+	// }
 
 	
 ?>
