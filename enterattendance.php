56d6839dbb22f8050e609a27512c67cac22ed49f
Karlo Espiritu <karlo.espiritu@yahoo.com>
2018-12-14 05:26:42 +0800

    update

--- enterattendance.php
+++ enterattendance.php
@@ -205,7 +205,7 @@
 						</div>
 						<div class="modal-footer">
 							<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-							<button type="button" class="btn btn-primary" data-dismiss="modal" id="saveAttEdit" onclick="saveAttEdit()">Save changes</button>
+							<button type="button" class="btn btn-primary" data-dismiss="modal" id="saveAttEdit">Save changes</button>
 						</div>
 					</div><!-- /.modal-content -->
 				</div><!-- /.modal-dialog -->
